c49e2e648ee017317326eb06f55dad55fb694144
derdoin <114993109+derdoin@users.noreply.github.com>
2023-11-23 21:50:17 +0300

    Create telegram.py

---
+++ telegram.py
@@ -0,0 +1,26 @@
+{
+    "ok": true,
+    "result": [
+        {
+            "update_id": 0,
+            "message": {
+                "message_id": 0,
+                "from": {
+                    "id": 0,
+                    "is_bot": false,
+                    "first_name": "",
+                    "last_name": "",
+                    "language_code": ""
+                },
+                "chat": {
+                    "id": 0,
+                    "first_name": "",
+                    "last_name": "",
+                    "type": ""
+                },
+                "date": 0,
+                "text": ""
+            }
+        }
+    ]
+}
